5cf29cc0b6086bd9d0326fea1f8936f5365664ba
kingsy <2716255013@qq.com>
2023-11-08 21:00:00 +0800

    Changes

--- IdeaDemo/src/com/atgu/idea/helloworld.java
+++ IdeaDemo/src/com/atgu/idea/helloworld.java
@@ -3,5 +3,6 @@ package com.atgu.idea;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world");
     }
 }
