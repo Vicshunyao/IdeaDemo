c7a093dad01fb27ddefba50971a9bb2def4e3213
Kingshunyao <2716255013@qq.com>
2023-11-10 11:51:05 +0800

    注册条件

--- springboot-regisiter/src/main/java/com/example/aaa/config/CommonConfig.java
+++ springboot-regisiter/src/main/java/com/example/aaa/config/CommonConfig.java
@@ -2,6 +2,10 @@ package com.example.aaa.config;
 
 import cn.itcast.pojo.Country;
 import cn.itcast.pojo.Province;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,18 +21,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CommonConfig {
     //    注入country对象
+//    如果配置文件中配置指定信息，则注入，反之不注入
+    @ConditionalOnProperty(prefix = "country", name = {"name", "system"})
     @Bean
-    public Country country(){
-        return new Country();
+    public Country country(@Value("${country.name}") String name, @Value("${country.system}") String system) {
+        Country country = new Country();
+        country.setName(name);
+        country.setSystem(system);
+
+        return country;
     }
 
-//    对象默认的名字是方法名
+    //    对象默认的名字是方法名
 //    @Bean("aa")
 //    如果方法内部需要使用到ioc容器中已经存在的bean对象，那么只需要在方法中声明即可，springboot会自动的注入
+//
     @Bean
-    public Province province(Country country){
-        System.out.println( "province"+country);
-        return new Province();
-    }
 
+//    如果ioc容器中不存在country，则注入province，否则不注入
+//    如果当前引入了web起步依赖，则环境中有DispatcherServlet,否则没有
+//    @ConditionalOnMissingBean(Country.class)
+    @ConditionalOnClass(name= "org.springframework.web.servlet.DispatcherServlet")
+    public Province province() {
+               return new Province();
+    }
 }
+//    public Province province(Country country){
+////        System.out.println( "province"+country);
+//        return new Province();
+//    }
+
+//}
--- springboot-regisiter/src/main/java/com/example/springbootregisiter/SpringbootRegisiterApplication.java
+++ springboot-regisiter/src/main/java/com/example/springbootregisiter/SpringbootRegisiterApplication.java
@@ -21,8 +21,8 @@ public class SpringbootRegisiterApplication {
 
 
         ApplicationContext context =SpringApplication.run(SpringbootRegisiterApplication.class, args);
-        Country country = context.getBean(Country.class);
-        System.out.println(country);
+//        Country country = context.getBean(Country.class);
+//        System.out.println(country);
 //.sout输出打印指令
         System.out.println(context.getBean("province"));
     }
